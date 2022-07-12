e8f8e1b56b75e0fbc169415668c54c349a7e7723
Matthew Wei <matthewwei35@gmail.com>
2022-07-11 19:11:47 -0700

    feat(app): fix updatetask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -61,7 +61,7 @@ const updateTask = async (req, res) => {
       return res.status(404).json({ msg:`No task with id: ${taskID}` })
     }
 
-    res.status(200).json({  })
+    res.status(200).json({ task })
   } catch (error) {
     res.status(500).json({ msg: error })
   }
